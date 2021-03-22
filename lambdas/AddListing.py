de5b9dff06fd664be2d91e91bd22b4ad183e2996
Vanthournut <47440446+Vanthournut@users.noreply.github.com>
2021-03-21 21:58:51 -0700

    Update AddListing.py

--- lambdas/AddListing.py
+++ lambdas/AddListing.py
@@ -1,14 +1,13 @@
 import json
 import boto3
 from botocore.exceptions import ClientError
+import uuid
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('BookBankListings')
-listingIndex = 0
-
 
 def lambda_handler(event, context):
-
+    
     error = ''
     try:
         if not event['username']:
@@ -24,17 +23,18 @@ def lambda_handler(event, context):
             'statusCode': 400,
             'body': json.dumps('Bad event call.')
         }
-
+        
     if error != '':
         return {
             'statusCode': 400,
             'body': json.dumps('Could not add listing:\n' + error)
         }
-
-    global listingIndex
+        
+    listingID = (uuid.uuid4().int >> 64)
+    
     response = table.put_item(
         Item={
-            'ListingID': listingIndex,
+            'ListingID': listingID,
             'Username': event['username'],
             'Location': event['location'],
             'Book': event['book'],
@@ -43,17 +43,14 @@ def lambda_handler(event, context):
             'Duration': event['duration']
         }
     )
-
-    listingIndex += 1
-
-    # TODO
+    
+    # TODO    
     # Reject Requests to this listing
-
+        
     return {
         'statusCode': 200,
-        'body': json.dumps('Successfully add listing ' + str(listingIndex-1) + '.')
+        'body': json.dumps('Successfully add listing ' + str(listingID) + '.')
     }
-
 
 def getBookInfo(event):
     if 'bookInfo' in event.keys():
