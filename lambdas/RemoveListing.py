57f5b84ecb7d737fc3c3bb4e65ce95fae9596f0a
Vanthournut <47440446+Vanthournut@users.noreply.github.com>
2021-04-08 20:58:18 -0700

    Update RemoveListing.py

--- lambdas/RemoveListing.py
+++ lambdas/RemoveListing.py
@@ -6,12 +6,30 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('BookBankListings')
 
 def lambda_handler(event, context):
+    
+    if not event['username']:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Invalid username.')
+        }
+    
     if event['listingID'] and str(event['listingID']).isnumeric():
+        
+        exprAttrVals = {}
+        exprAttrNames = {}
+        
+        filterExpression = "#" + "username = :username"
+        exprAttrNames['#username'] = 'Username'
+        exprAttrVals[':username'] = event['username']
+        
         try:
             response = table.delete_item(
                 Key={
                     'ListingID': str(event['listingID'])
-                }
+                },
+                ConditionExpression=filterExpression,
+                ExpressionAttributeNames=exprAttrNames,
+                ExpressionAttributeValues=exprAttrVals
             )
         except ClientError as e:
             return {
